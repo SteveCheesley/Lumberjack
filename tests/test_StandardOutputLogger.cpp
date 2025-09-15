4317e9b2ed8b26b4d8f26bef71daafa85d1dfd94
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-09-15 21:03:41 +0100

    Adding a working set of basic unit tests for the current standard output logger.

---
+++ tests/test_StandardOutputLogger.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include "Logger.h"
+#include "StandardOutputLogger.h"
+
+using getmybytes::logger::Logger;
+using getmybytes::logger::LogLevel;
+using getmybytes::logger::StandardOutputLogger;
+using getmybytes::logger::LoggerConfiguration;
+
+class StandardOutputLoggerTester : public testing::Test {
+    protected:
+        std::streambuf* originalCoutBuffer = std::cout.rdbuf();
+        std::stringstream buffer;
+
+        Logger* logger;
+
+        StandardOutputLoggerTester() {
+            // SetUp
+            originalCoutBuffer = std::cout.rdbuf();
+            std::cout.rdbuf(buffer.rdbuf());
+        }
+
+        ~StandardOutputLoggerTester() {
+            // TearDown
+            std::cout.rdbuf(originalCoutBuffer);
+
+        }
+
+        std::string getCapturedOutput() const {
+            return buffer.str();
+        }
+
+        void clearCapturedOutput() {
+            buffer.str("");
+            buffer.clear();
+        }
+};
+
+TEST_F(StandardOutputLoggerTester, WritesBasicLogEntry) {
+    logger = new StandardOutputLogger(new LoggerConfiguration(
+        false, 
+        false, 
+        LogLevel::ERROR));
+    logger->log(LogLevel::INFO, "Hello, world!");
+
+    std::string output = getCapturedOutput();
+
+    EXPECT_EQ(output, "INFO  :: Hello, World!");
+}
+
+TEST_F(StandardOutputLoggerTester, DoesNotWriteInvalidLevel) {
+    logger = new StandardOutputLogger(new LoggerConfiguration(
+        false, 
+        false, 
+        LogLevel::INFO));
+    logger->log(LogLevel::ERROR, "Hello, world!");
+
+    std::string output = getCapturedOutput();
+
+    ASSERT_TRUE(output.empty());
+}
