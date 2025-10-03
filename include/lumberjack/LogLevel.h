06deac748fed93ebcced31f590e10f75d2ae4ee7
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-10-03 16:10:53 +0100

    Putting the LogLevel enum out into it's own header file. This will make it easier to reference from multiple locations.

---
+++ include/lumberjack/LogLevel.h
@@ -0,0 +1,16 @@
+#ifndef LOGLEVEL_H
+#define LOGLEVEL_H
+
+namespace lumberjack 
+{
+    enum LogLevel 
+    {
+        ERROR,
+        WARNING,
+        INFO,
+        DEBUG,
+        TRACE
+    };
+}
+
+#endif
