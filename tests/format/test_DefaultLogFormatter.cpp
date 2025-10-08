5e70605270a7de50196d46e9220e3bde384e4db9
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-10-08 18:31:06 +0100

    Adding a fully functioning success scenario unit test for the DefaultLogFormatter implementation.

--- tests/format/test_DefaultLogFormatter.cpp
+++ tests/format/test_DefaultLogFormatter.cpp
@@ -1,13 +1,20 @@
 #include <gtest/gtest.h>
 #include <string>
 #include "lumberjack/format/DefaultLogFormatter.h"
+#include "../time/MockTimeProvider.h"
 
 using lumberjack::format::DefaultLogFormatter;
 using lumberjack::format::ILogFormatter;
+using lumberjack::time::ITimeProvider;
+using lumberjack::time::MockTimeProvider;
+using lumberjack::format::LogMessageFormat;
 using lumberjack::LogLevel;
 
 TEST(DefaultLogFormatterTest, HandlesStandardInput) {
-    ILogFormatter* subject = new DefaultLogFormatter();
+    auto fixedTime = std::chrono::system_clock::from_time_t(1609459200);
+    ITimeProvider* mockTimeProvider = new MockTimeProvider(fixedTime);
+
+    ILogFormatter* subject = new DefaultLogFormatter(LogMessageFormat(true, true, true), mockTimeProvider);
     
     std::string result = subject->formatMessage(LogLevel::INFO, "Hello World!");
 
