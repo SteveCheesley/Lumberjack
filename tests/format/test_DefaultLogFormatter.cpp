eebf3c2a301b0518674369f2e389bf6e386b7351
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-10-09 21:15:36 +0100

    Upgrading to reduce duplication by allowing for test fixtures to be specified. This will support multiple tests for unhappy path testing.

--- tests/format/test_DefaultLogFormatter.cpp
+++ tests/format/test_DefaultLogFormatter.cpp
@@ -10,35 +10,32 @@ using lumberjack::time::MockTimeProvider;
 using lumberjack::format::LogMessageFormat;
 using lumberjack::LogLevel;
 
-TEST(DefaultLogFormatterTest, BasicSuccessTest) {
-    auto fixedTime = std::chrono::system_clock::from_time_t(1609459200);
-    ITimeProvider* mockTimeProvider = new MockTimeProvider(fixedTime);
+struct TimeHelper {
 
-    ILogFormatter* subject = new DefaultLogFormatter(LogMessageFormat(true, true, true), mockTimeProvider);
+};
+
+class DefaultLogFormatterTest : public ::testing::Test {
+    protected:
+        ITimeProvider* mockTimeProvider;
+
+        // Called before *each* test
+        void SetUp() override {
+            auto fixedTime = std::chrono::system_clock::from_time_t(1609459200);
+            mockTimeProvider = new MockTimeProvider(fixedTime);
+        }
+
+        // Called after *each* test
+        void TearDown() override {
+        }
+};
+
+
+TEST_F(DefaultLogFormatterTest, BasicSuccessTest) {
+    ILogFormatter* subject = new DefaultLogFormatter(
+        LogMessageFormat(true, true, true), 
+        mockTimeProvider);
     
     std::string result = subject->formatMessage(LogLevel::INFO, "Hello World!");
 
     EXPECT_EQ(result, "2025-10-04 20:39.921 [INFO] Hello, World!");
-}
-
-TEST(DefaultLogFormatterTest, CanRestrictLevel) {
-    auto fixedTime = std::chrono::system_clock::from_time_t(1609459200);
-    ITimeProvider* mockTimeProvider = new MockTimeProvider(fixedTime);
-
-    ILogFormatter* subject = new DefaultLogFormatter(LogMessageFormat(true, true, false), mockTimeProvider);
-    
-    std::string result = subject->formatMessage(LogLevel::INFO, "Hello World!");
-
-    EXPECT_EQ(result, "2025-10-04 20:39.921 Hello, World!");
-}
-
-TEST(DefaultLogFormatterTest, CanRestrictTime) {
-    auto fixedTime = std::chrono::system_clock::from_time_t(1609459200);
-    ITimeProvider* mockTimeProvider = new MockTimeProvider(fixedTime);
-
-    ILogFormatter* subject = new DefaultLogFormatter(LogMessageFormat(true, false, false), mockTimeProvider);
-    
-    std::string result = subject->formatMessage(LogLevel::INFO, "Hello World!");
-
-    EXPECT_EQ(result, "2025-10-04 Hello, World!");
 }
