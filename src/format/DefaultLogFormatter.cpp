fd203918a2a5cb2331228a9ad52928bc45043530
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-10-26 09:08:46 +0000

    Adding a working implementation for date and time printing in the default log formatter. This code isn't pretty and isn't designed for production use. I feel that it might be too tightly coupled at this stage. The next thing to do is to get the remaining logic working and then refactor so that it isn't so tightly coupled.

--- src/format/DefaultLogFormatter.cpp
+++ src/format/DefaultLogFormatter.cpp
@@ -1,3 +1,8 @@
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+// #include <iostream>
 #include "lumberjack/format/DefaultLogFormatter.h"
 #include "lumberjack/time/SystemTimeProvider.h"
 
@@ -24,7 +29,52 @@ namespace lumberjack::format
 
     std::string DefaultLogFormatter::formatMessage(LogLevel logLevel, std::string message)
     {
-        // TODO - Formatting logic goes here
-        return "output-goes-here";
+        std::string output = "";
+
+        std::string dateString = "";
+        std::string timeString = "";
+        std::string sourceString = "";
+
+        if (logMessageFormat.isDatePrinted()) {
+            // Append the date
+            std::chrono::system_clock::time_point now = timeProvider->now();
+            std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+            // Convert to UTC-based tm structure
+            std::tm* utc_time = std::gmtime(&time);
+
+            // Use this string stream to build the entire output
+            std::ostringstream dateStringStream;
+            dateStringStream << std::put_time(utc_time, "%Y-%m-%d");
+
+            dateString = dateStringStream.str();
+        }
+
+        // NOTE: This code is terrible, but I want to understand better how time_point works..
+        // TODO - This needs to go in a private method - This could actually be a design pattern - better
+        if (logMessageFormat.isTimePrinted()) {
+            // Append the date
+            std::chrono::system_clock::time_point now = timeProvider->now();
+            std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+            // Convert to UTC-based tm structure
+            std::tm* utc_time = std::gmtime(&time);
+
+            // Use this string stream to build the entire output
+            std::ostringstream timeStringStream;
+            timeStringStream << std::put_time(utc_time, "%H:%M:%S");
+
+            timeString = timeStringStream.str();
+        }
+
+        if (logMessageFormat.isSourcePrinted()) {
+            // TODO - Figure out how to get this here...
+        }
+
+        output = dateString + " " + 
+                 timeString + " " + 
+                 sourceString + message;
+        
+        return output;
     }
 };
