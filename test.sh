3afcbb160a29ba6a3178a2f5227daf800bdfb5de
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-10-26 08:50:10 +0000

    Upgrading the test script to allow individual unit tests to be executed. This is useful when I am trying to debug a single problem.

--- test.sh
+++ test.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # Compile sources (both lib and test)
+echo "..:: Building resources to include latest changes ::.."
 ./build.sh
 
-# Execute tests
-ctest --test-dir build --output-on-failure
+while getopts t: flag
+do
+    case "${flag}" in
+        t) testFlag=${OPTARG};;
+    esac
+done
+
+if [ -n "$testFlag" ] 
+then
+    echo "..:: Executing '$testFlag' ::.."
+    ./build/lumberjack_tests --gtest_filter=$testFlag
+else
+    # Execute tests
+    echo "..:: Executing all tests in the suite ::.."
+    ctest --test-dir build --output-on-failure
+fi
+echo "..:: Test Execution Complete ::..
