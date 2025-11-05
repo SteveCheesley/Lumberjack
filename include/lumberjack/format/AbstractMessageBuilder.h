9f70974d9e3d1982930bb0a7ec56458a9339a538
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-11-05 21:33:54 +0000

    Adding the "base" abstract implementation of a link in the chain of responsibility for building the log message. This shares common functionality with the various links and reduces repetition.

---
+++ include/lumberjack/format/AbstractMessageBuilder.h
@@ -0,0 +1,23 @@
+#ifndef ABSTRACTMESSAGEBUILDER_H
+#define ABSTRACTMESSAGEBUILDER_H
+
+#include "IMessageBuilder.h"
+
+namespace lumberjack::format
+{
+    class AbstractMessageBuilder : public IMessageBuilder {
+        private:
+            IMessageBuilder *nextHandler;
+
+        public:
+            AbstractMessageBuilder() : nextHandler(nullptr) {
+            }
+            IMessageBuilder *setNext(IMessageBuilder *messageBuilder) override;
+            virtual bool canAddToMessage() = 0;
+            virtual std::string doMessageBuild() = 0;
+
+            std::string buildMessage() override final;
+    };
+}
+
+#endif
---
+++ src/format/AbstractMessageBuilder.cpp
@@ -0,0 +1,26 @@
+#include "lumberjack/format/AbstractMessageBuilder.h"
+
+namespace lumberjack::format
+{
+    IMessageBuilder *AbstractMessageBuilder::setNext(IMessageBuilder *messageBuilder)
+    {
+        this->nextHandler = messageBuilder;
+    }
+
+    std::string AbstractMessageBuilder::buildMessage()
+    {
+        std::string message = "";
+
+        if (this->nextHandler)
+        {
+            message = this->nextHandler->buildMessage();
+        }
+
+        if (this->canAddToMessage())
+        {
+            message = message + this->doMessageBuild();
+        }
+
+        return message;
+    }
+}
