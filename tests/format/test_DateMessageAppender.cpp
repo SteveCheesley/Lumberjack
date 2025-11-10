de634706e1a3407de763f8faedb317b603a64d53
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-11-10 18:18:44 +0000

    Adding a working unit test for the happy path functionality of the DateMessageAppender. This will allow for the formatted output to be tested and verified.

--- tests/format/test_DateMessageAppender.cpp
+++ tests/format/test_DateMessageAppender.cpp
@@ -2,21 +2,31 @@
 #include <gtest/gtest.h>
 #include "lumberjack/format/DateMessageAppender.h"
 #include "lumberjack/LogLevel.h"
+#include "../time/MockTimeProvider.h"
 
-class DateMessageAppenderTest : public testing::Test {
-    protected:
-        lumberjack::format::DateMessageAppender* subject;
+class DateMessageAppenderTest : public testing::Test
+{
+protected:
+    lumberjack::time::MockTimeProvider* mockTimeProvider;
+    lumberjack::format::DateMessageAppender *subject;
+
+    void SetUp() override
+    {
+        auto fixedTime = std::chrono::system_clock::from_time_t(1759737600);
+        mockTimeProvider = new lumberjack::time::MockTimeProvider(fixedTime);
+    }
 };
 
-TEST_F(DateMessageAppenderTest, testBasicSuccess) {
+TEST_F(DateMessageAppenderTest, testBasicSuccess)
+{
     lumberjack::format::LogMessageFormat logMessageFormat(
-        true, 
+        true,
         false,
         false);
 
-    subject = new lumberjack::format::DateMessageAppender(logMessageFormat);
+    subject = new lumberjack::format::DateMessageAppender(logMessageFormat, mockTimeProvider);
 
     std::string result = subject->buildMessage();
 
-    EXPECT_EQ(result, "2025-10-04 ");
+    EXPECT_EQ(result, "2025-10-06 ");
 }
