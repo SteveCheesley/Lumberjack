1f189fd6ed6a3cc336ca4fb045d36c204522302b
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-11-10 18:57:21 +0000

    Adding a skeletal implementation of the Time message appender. Before the final logic is added, unit testing will be added to test the functionality of the link. This follows a test-driven development approach.

---
+++ include/lumberjack/format/TimeMessageAppender.h
@@ -0,0 +1,27 @@
+#ifndef TIMEMESSAGEAPPENDER_H
+#define TIMEMESSAGEAPPENDER_H
+
+#include "lumberjack/format/AbstractMessageBuilder.h"
+#include "lumberjack/format/LogMessageFormat.h"
+#include "lumberjack/time/ITimeProvider.h"
+
+namespace lumberjack::format
+{
+    class TimeMessageAppender : public AbstractMessageBuilder
+    {
+        private:
+            lumberjack::time::ITimeProvider* timeProvider;
+            const lumberjack::format::LogMessageFormat &logMessageFormat;
+        protected:
+            bool canAddToMessage() override final;
+            std::string doMessageBuild() override final;
+        public:
+            TimeMessageAppender() = delete;
+            TimeMessageAppender(const lumberjack::format::LogMessageFormat& logMessageFormat);
+            TimeMessageAppender(
+                const lumberjack::format::LogMessageFormat& logMessageFormat, 
+                lumberjack::time::ITimeProvider* timeProvider);
+    };
+}
+
+#endif
---
+++ src/format/TimeMessageAppender.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+
+#include "lumberjack/format/TimeMessageAppender.h"
+#include "lumberjack/format/LogMessageFormat.h"
+#include "lumberjack/time/SystemTimeProvider.h"
+#include "lumberjack/time/time_functions.h"
+
+namespace lumberjack::format
+{
+    TimeMessageAppender::TimeMessageAppender(const lumberjack::format::LogMessageFormat &inputLogMessageFormat)
+        : TimeMessageAppender(inputLogMessageFormat, new lumberjack::time::SystemTimeProvider())
+    {
+    }
+
+    TimeMessageAppender::TimeMessageAppender(
+        const lumberjack::format::LogMessageFormat &inputLogMessageFormat,
+        lumberjack::time::ITimeProvider *timeProvider) : AbstractMessageBuilder(),
+        logMessageFormat(inputLogMessageFormat),
+        timeProvider(timeProvider)
+    {
+    }
+    bool TimeMessageAppender::canAddToMessage()
+    {
+        return false;
+    }
+
+    std::string TimeMessageAppender::doMessageBuild()
+    {
+        return "";
+    }
+}
