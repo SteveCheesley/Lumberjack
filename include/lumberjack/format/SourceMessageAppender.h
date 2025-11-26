b716a857fb6d790a5895b674bcb7b9efa2b1b484
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-11-26 20:43:16 +0000

    Adding a new SourceMessageAppender. This appender class is part of the Message Builder chain of responsibility and allows us to use an object oriented structure to define the format of a message.

---
+++ include/lumberjack/format/SourceMessageAppender.h
@@ -0,0 +1,22 @@
+#ifndef SOURCEMESSAGEAPPENDER_H
+#define SOURCEMESSAGEAPPENDER_H
+
+#include "lumberjack/format/AbstractMessageBuilder.h"
+#include "lumberjack/format/LogMessageFormat.h"
+
+namespace lumberjack::format
+{
+    class SourceMessageAppender : public AbstractMessageBuilder
+    {
+        private:
+            const lumberjack::format::LogMessageFormat &LogMessageFormat;
+        protected:
+            bool canAddToMessage() override final;
+            std::string doMessageBuild() override final;
+        public:
+            SourceMessageAppender() = delete;
+            SourceMessageAppender(const lumberjack::format::LogMessageFormat& logMessageFormat);
+    };
+}
+
+#endif
