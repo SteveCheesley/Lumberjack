300207ff47fdd168b298bc5437afc3fd45ce8639
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-12-04 20:47:49 +0000

    Adds a working test for the LogLevelMessageAppender  class. These tests validate both output format and the gateway function.

---
+++ tests/format/builder/test_LogLevelMessageAppender.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "lumberjack/format/builder/LogLevelMessageAppender.h"
+
+class LogLevelMessageAppenderTest : public testing::Test
+{
+    protected:
+        std::unique_ptr<lumberjack::format::LogMessageFormat> testFormatSpecification;
+        std::unique_ptr<lumberjack::format::builder::MessageBuilderInput> testBuildMessageInput;
+
+        void SetUp() override
+        {
+            testFormatSpecification = std::make_unique<lumberjack::format::LogMessageFormat>(
+                true,
+                true,
+                true,
+                true
+            );
+
+            testBuildMessageInput = std::make_unique<lumberjack::format::builder::MessageBuilderInput>(
+                typeid(LogLevelMessageAppenderTest)
+            );
+        }
+};
+
+TEST_F(LogLevelMessageAppenderTest, basicSuccessTest)
+{
+    lumberjack::format::builder::LogLevelMessageAppender subject(*testFormatSpecification);
+
+    std::string result = subject.buildMessage(testBuildMessageInput.get());
+
+    EXPECT_EQ(result, "[INFO] ");
+}
+
+TEST_F(LogLevelMessageAppenderTest, isOutputEmptyIfInactive)
+{
+    lumberjack::format::LogMessageFormat deactivatedFormat(false, false, false, false);
+    lumberjack::format::builder::LogLevelMessageAppender subject(*testFormatSpecification);
+
+    std::string result = subject.buildMessage(testBuildMessageInput.get());
+
+    EXPECT_EQ(result, "");
+}
