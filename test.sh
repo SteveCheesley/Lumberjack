61bf259c620972d73be76405f3831b570630ac95
Stephen Cheesley <stephen.cheesley@gmail.com>
2025-12-29 23:06:19 +0000

    Upgrading the test.sh script to allow existing unit tests to be listed. This will make it easier for other developers to easily identify tests that that they might want to execute using the gtest filter.

--- test.sh
+++ test.sh
@@ -4,20 +4,32 @@
 echo "..:: Building resources to include latest changes ::.."
 ./build.sh
 
-while getopts t: flag
-do
-    case "${flag}" in
-        t) testFlag=${OPTARG};;
-    esac
+# Initialise Variables
+t_arg=""
+l_flag=0
+
+# Parse the options
+while getopts "t:l" opt; do
+  case $opt in
+    t)
+      t_arg=$OPTARG
+      ;;
+    l)
+      l_flag=1
+      ;;
+  esac
 done
 
-if [ -n "$testFlag" ] 
-then
-    echo "..:: Executing '$testFlag' ::.."
-    ./build/lumberjack_tests --gtest_filter=$testFlag
+# Determine the operation based on the flags provided
+if [ $l_flag -eq 1 ]; then
+    ./build/lumberjack_tests --gtest_list_tests
+    exit 0
+elif [ -n "$t_arg" ]; then
+    echo "..:: Executing '$t_arg' ::.."
+    ./build/lumberjack_tests --gtest_filter=$t_arg
+    exit 0
 else
-    # Execute tests
     echo "..:: Executing all tests in the suite ::.."
     ctest --test-dir build --output-on-failure
+    exit 0;
 fi
-echo "..:: Test Execution Complete ::.."
