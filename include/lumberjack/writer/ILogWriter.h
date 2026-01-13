f5e8f281c063a1dbfd3199b5830c88acf85e189f
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-01-13 21:43:14 +0000

    Creates a new ILogWriter interface. This has several new features that I have recently learned about. Thanks to the introduction of a linter I have learned about more modern C++ standards, including the use of trailing return types. I have also learned about best practice around the specific management of copy and move constructors and assignments. Deleting these functions helps insure that the interface itself cannot be copied or moved.

---
+++ include/lumberjack/writer/ILogWriter.h
@@ -0,0 +1,21 @@
+#ifndef ILOGWRITER_H
+#define ILOGWRITER_H
+
+#include <string>
+
+namespace lumberjack::writer
+{
+    class ILogWriter
+    {
+        public:
+            virtual void writeToLog(std::string entry) = 0;
+            virtual ~ILogWriter() = default;
+
+            ILogWriter(const ILogWriter&) = delete;
+            auto operator=(const ILogWriter&) -> ILogWriter& = delete;
+            ILogWriter(ILogWriter&&) = delete;
+            auto operator=(ILogWriter&&) -> ILogWriter& = delete;
+    };
+}
+
+#endif
