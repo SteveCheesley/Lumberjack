ea07058e54c5e2c0654310bde471b188549663a5
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-01-15 21:03:04 +0000

    Adding a comment to justify the use of printf over cout in this instance. I don't usually like peppering code with comments, but in this case I felt this was worth while.

--- src/writer/StandardOutputWriter.cpp
+++ src/writer/StandardOutputWriter.cpp
@@ -5,6 +5,9 @@ namespace lumberjack::writer
 {
     void StandardOutputWriter::writeToLog(const std::string& entry) 
     {
+        /* === Use of printf
+         * I've opted to use printf over cout to try to optimise performance 
+         * and reduce both memory and execution overheads. */
         printf(entry.data(), 0);
     }
 }
