2dfda59b0029b41db77c8e3ea4ef239442b02645
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-01-27 21:57:57 +0000

    Adding a mock ILogFormatter implementation. This works to allow testing of the LogConfiguration that has the ILogFormatter as one of its attributes. There are some linter errors on the MockLogFormatter, but some of them are uncorrectable until the interface is addressed. I felt it pragmatic to move past these since test code isn't included in the CI linter execution.

---
+++ tests/format/MockLogFormatter.h
@@ -0,0 +1,32 @@
+#ifndef MOCKLOGFORMATTER_H
+#define MOCKLOGFORMATTER_H
+
+#include <utility>
+
+#include "lumberjack/format/ILogFormatter.h"
+
+namespace lumberjack::format
+{
+    class MockLogFormatter : public ILogFormatter 
+    {
+        private:
+            std::string expectedOutput_;
+            int invocationCount_ = 0;
+        public:
+            MockLogFormatter(std::string expectedOutput) : expectedOutput_(std::move(expectedOutput)) {}
+            virtual ~MockLogFormatter() = default;
+
+            std::string formatMessage(std::type_index source, lumberjack::LogLevel level, std::string message)
+            {
+                this->invocationCount_++;
+                return this->expectedOutput_;
+            }
+
+            [[nodiscard]] auto getInvocationCount() const
+            {
+                return this->invocationCount_;
+            }
+    };
+}
+
+#endif
