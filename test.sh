e00342d37329fbeccbac73a7fb2f4178f2a9a688
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-02-09 21:59:38 +0000

    Fixing issue where the test script does not return the correct exit codes from executing unit tests. This was causing CI to mask unit test failures.

--- test.sh
+++ test.sh
@@ -31,13 +31,13 @@ fi
 # Determine the operation based on the flags provided
 if [ $l_flag -eq 1 ]; then
     ./build/lumberjack_tests --gtest_list_tests
-    exit 0
+    exit $?
 elif [ -n "$t_arg" ]; then
     echo "..:: Executing '$t_arg' ::.."
     ./build/lumberjack_tests --gtest_filter=$t_arg
-    exit 0
+    exit $?
 else
     echo "..:: Executing all tests in the suite ::.."
     ctest --test-dir build --output-on-failure
-    exit 0;
+    exit $?
 fi
