cda606280a70a8069f4213f6c8d174d7b6c263bf
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-02-11 22:29:01 +0000

    Switching to a vector in the LogConfigurations. I don't need the expensive index a map provides and following Bjarns advice. Always start with a vector.

--- include/lumberjack/configuration/LogConfigurations.h
+++ include/lumberjack/configuration/LogConfigurations.h
@@ -1,7 +1,7 @@
 #ifndef LOGCONFIGURATIONS_H
 #define LOGCONFIGURATIONS_H
 
-#include <map>
+#include <vector>
 #include <string>
 #include "lumberjack/configuration/LogConfiguration.h"
 
@@ -11,10 +11,15 @@ namespace lumberjack::configuration
     {
         private:
             LogConfiguration default_;
-            std::map<std::string, LogConfiguration> configurations_;
+            std::vector<LogConfiguration> configurations_;
         public:
             LogConfigurations();
+            LogConfigurations(LogConfiguration defaultConfiguration);
+            LogConfigurations(std::vector<LogConfiguration> configurations);
+            LogConfigurations(LogConfiguration defaultConfiguration, std::vector<LogConfiguration> configurations);
             void log(std::type_index source, lumberjack::LogLevel logLevel, const std::string& message);
+            void add(LogConfiguration LogConfiguration);
+            void clearAll();
     };
 }
 
