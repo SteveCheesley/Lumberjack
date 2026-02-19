f5d43723c009e9b65635e553cd226ae4b28b3294
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-02-19 21:40:06 +0000

    Adding a header class definition for the Logger class that will be used in classes and free-functions to triger writes to the logs.

---
+++ include/lumberjack/Logger.h
@@ -0,0 +1,32 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <typeindex>
+#include <string>
+#include "lumberjack/LogLevel.h"
+
+namespace lumberjack
+{
+    class Logger
+    {
+        private:
+            std::type_index source;
+        public:
+            constexpr Logger(std::type_index source);
+            ~Logger();
+
+            Logger(const Logger&) = delete;
+            auto operator=(const Logger&) -> Logger& = delete;
+            Logger(Logger&&) = delete;
+            auto operator=(Logger&&) -> Logger& = delete;
+
+            void log(LogLevel logLevel, std::string logMessage);
+            void info(std::string message);
+            void warn(std::string message);
+            void error(std::string message);
+            void debug(std::string message);
+            void trace(std::string message);
+    };    
+}
+
+#endif
