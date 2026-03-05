5cf1a2d039833425732bb2c2e94de36c499eb70f
Stephen Cheesley <stephen.cheesley@gmail.com>
2026-03-05 21:58:40 +0000

    Adding test initialisation code for the Logger integration test. This sets up an appropriate log configuration for this test. This works nicely as documentation for how to integrate logging into a C++ application. At the time of commit this is not in a functioning state.

--- tests/integration/test_Logger.cpp
+++ tests/integration/test_Logger.cpp
@@ -1,10 +1,56 @@
 #include <gtest/gtest.h>
+#include "lumberjack/logging.h"
 #include "lumberjack/Logger.h"
+#include "lumberjack/format/DefaultLogFormatter.h"
+#include "lumberjack/writer/StandardOutputWriter.h"
+#include "lumberjack/configuration/LogConfiguration.h"
+#include "lumberjack/configuration/LogConfigurations.h"
 #include "../format/MockLogFormatter.h"
+
+class GlobalTestEnvironment : public ::testing::Environment 
+{
+    public:
+        void SetUp() override {
+            // --== Runs once before all tests
+            // Log Formatter
+            std::unique_ptr<lumberjack::format::ILogFormatter> defaultLogFormatter = 
+                std::make_unique<lumberjack::format::DefaultLogFormatter>();
+            
+            // Log Writer
+            std::vector<std::unique_ptr<lumberjack::writer::ILogWriter>> logWriters;
+            logWriters.push_back(std::make_unique<lumberjack::writer::StandardOutputWriter>());
+
+            // Log Configuration
+            lumberjack::configuration::LogConfiguration testLogConfiguration(
+                lumberjack::LogLevel::INFO, 
+                std::move(defaultLogFormatter), 
+                std::move(logWriters));
+
+            // Log Configuraitons
+            std::vector<lumberjack::configuration::LogConfiguration> logConfigurationVector;
+            logConfigurationVector.push_back(std::move(testLogConfiguration));
+
+            std::unique_ptr<lumberjack::configuration::LogConfigurations<lumberjack::configuration::LogConfiguration>> logConfigurations = 
+                std::make_unique<lumberjack::configuration::LogConfigurations<lumberjack::configuration::LogConfiguration>>(logConfigurationVector);
+
+            lumberjack::set_logConfigurations(std::move(logConfigurations));
+        }
+
+        void TearDown() override {
+            // --== Runs once after all tests
+        }
+
+        void setupLogConfigurations() {
+            
+        };
+};
 
 TEST(LoggerTest, WritesToConsole)
 {
     lumberjack::Logger subject(typeid(lumberjack::format::MockLogFormatter));
 
-    subject.info("Hello World..");
+    subject.warn("...::: Hello World ===|||=== :::...");
+
+    // Now that I know this is working and that the issue was the wiring threshold I can set up a 
+    // working example.
 }
